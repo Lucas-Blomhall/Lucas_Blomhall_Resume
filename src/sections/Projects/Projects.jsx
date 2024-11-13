71af4ac8104c383b9446c1b9fd6147a0cdae7b29
Lucas-Blomhall <lucas.blomhall@gmail.com>
2024-11-14 00:55:15 +0100

    Edited extra nice

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -12,25 +12,25 @@ function Projects() {
       <div className={styles.projectsContainer}>
         <ProjectCard
           src={viberr}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://www.linkedin.com/posts/lucas-blomhall_fullstackdevnetcloudtechnician-networkautomation-activity-7260634907687743488-JFjr?utm_source=share&utm_medium=member_desktop"
           h3="Cisco 1921 Router"
           p="Cisco Router"
         />
         <ProjectCard
           src={freshBurger}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://www.linkedin.com/company/the-full-stack-devnet-cloud-blog/?viewAsMember=true"
           h3="Blog on LinkedIn"
           p="Sharing my journey"
         />
         <ProjectCard
           src={hipsster}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://www.comptia.org/certifications/network"
           h3="CompTIA Network+"
           p="In progress"
         />
         <ProjectCard
           src={fitLift}
-          link="https://github.com/Ade-mir/company-landing-page-2"
+          link="https://www.linkedin.com/posts/lucas-blomhall_im-happy-to-share-this-my-high-school-graduation-activity-7119276674608709633-4fGJ?utm_source=share&utm_medium=member_desktop"
           h3="Gymnasiearbete"
           p="Arduino robot"
         />
