1fe1463cc081032bd32cd001f5ed6610d6caf23d
Lucas-Blomhall <lucas.blomhall@gmail.com>
2024-11-14 11:18:35 +0100

    edit skills and changed cv

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -20,7 +20,7 @@ function Skills() {
       </div>
       <hr />
       <div className={styles.skillList}>
-        <SkillList src={checkMarkIcon} skill="React" />
+        <SkillList src={checkMarkIcon} skill="React.js" />
         <SkillList src={checkMarkIcon} skill="Angular" />
         <SkillList src={checkMarkIcon} skill="Vue" />
         <SkillList src={checkMarkIcon} skill="Tailwind CSS" />
@@ -39,7 +39,7 @@ function Skills() {
         <SkillList src={checkMarkIcon} skill="CSS" />
         <SkillList src={checkMarkIcon} skill="JavaScript" />
         <SkillList src={checkMarkIcon} skill="TypeScript" />
-        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="Node.js" />
       </div>
     </section>
   );
