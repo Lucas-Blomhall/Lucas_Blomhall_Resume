eca2aebe629941e93b061f76ffbc52ee200a30e6
Lucas-Blomhall <lucas.blomhall@gmail.com>
2024-11-14 11:55:15 +0100

    email service works

--- src/sections/Contact/Contact.jsx
+++ src/sections/Contact/Contact.jsx
@@ -1,6 +1,30 @@
+import React, { useRef } from 'react';
 import styles from './ContactStyles.module.css';
+import emailjs from '@emailjs/browser';
+
+// För att kunna skicka emails till sig själv installera: "npm i @emailjs/browser"
 
 function Contact() {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm('service_1syjzfi', 'template_hac19h9', form.current, {
+        publicKey: 'v1tuK24Y8Urg7VrA4',
+      })
+      .then(
+        () => {
+          console.log('SUCCESS!');
+          console.log("message sent");
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+  
   return (
     <section id="contact" className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
@@ -40,6 +64,15 @@ function Contact() {
             required></textarea>
         </div>
         <input className="hover btn" type="submit" value="Submit" />
+      </form>
+      <form ref={form} onSubmit={sendEmail}>
+        <label>Name</label>
+        <input type="text" name="user_name" />
+        <label>Email</label>
+        <input type="email" name="user_email" />
+        <label>Message</label>
+        <textarea name="message" />
+        <input type="submit" value="Send" />
       </form>
     </section>
   );
