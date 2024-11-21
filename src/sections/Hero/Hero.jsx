da4d8cf18afdd9f89b5fa47f0ee9cf549fc51e06
Lucas-Blomhall <lucas.blomhall@gmail.com>
2024-11-21 13:16:24 +0100

    ny bild ska upp på LinkedIn igen nytt försök

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import styles from './HeroStyles.module.css';
-import heroImg from '../../assets/LinkedIn profile picture-modified.png';
+import heroImg from '../../assets/LinkedIn profile picture-modified circle.png';
 import sun from '../../assets/sun.svg';
 import moon from '../../assets/moon.svg';
 import twitterLight from '../../assets/twitter-light.svg';
